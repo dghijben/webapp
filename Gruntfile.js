cd8722e08bf7cc3ed3fa017160df6c07a362027e
Dave Ghijben <dghijben@gmail.com>
2014-07-19 11:17:05 +0900

    whatever

--- Gruntfile.js
+++ Gruntfile.js
@@ -286,28 +286,28 @@ module.exports = function (grunt) {
         // By default, your `index.html`'s <!-- Usemin block --> will take care of
         // minification. These next options are pre-configured if you do not wish
         // to use the Usemin blocks.
-        // cssmin: {
-        //     dist: {
-        //         files: {
-        //             '<%= config.dist %>/styles/main.css': [
-        //                 '.tmp/styles/{,*/}*.css',
-        //                 '<%= config.app %>/styles/{,*/}*.css'
-        //             ]
-        //         }
-        //     }
-        // },
-        // uglify: {
-        //     dist: {
-        //         files: {
-        //             '<%= config.dist %>/scripts/scripts.js': [
-        //                 '<%= config.dist %>/scripts/scripts.js'
-        //             ]
-        //         }
-        //     }
-        // },
-        // concat: {
-        //     dist: {}
-        // },
+        cssmin: {
+             dist: {
+                 files: {
+                     '<%= config.dist %>/styles/main.css': [
+                         '.tmp/styles/{,*/}*.css',
+                         '<%= config.app %>/styles/{,*/}*.css'
+                     ]
+                 }
+             }
+        },
+        uglify: {
+             dist: {
+                 files: {
+                     '<%= config.dist %>/scripts/scripts.js': [
+                         '<%= config.dist %>/scripts/scripts.js'
+                     ]
+                 }
+             }
+        },
+        concat: {
+             dist: {}
+        },
 
         // Copies remaining files to places other tasks can use
         copy: {
@@ -431,7 +431,7 @@ module.exports = function (grunt) {
         'concurrent:dist',
         'autoprefixer',
         'concat',
-        //'cssmin',
+        'cssmin',
         'uglify',
         'copy:dist',
         'modernizr',
